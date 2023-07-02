d38aedc5a4b357bf82272a4e129b5876e1c44c01
Jackie Tan <jackilyn.tan@gmail.com>
2023-07-02 19:52:37 -0400

    Heroku

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Create a new Sequelize instance and connect to the database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = process.env.JAWSDB_URL ? 
+  new Sequelize(process.env.JAWSDB_URL) :
+  new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'mysql',
